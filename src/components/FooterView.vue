07ee90bcf277630a9b3c90e5de045126077c7516
yuling <gb2013429@gmail.com>
2024-12-23 16:26:50 +0800

    add-header-link + add-footer-link

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="text-center mb-6 bg-white mx-4" style="border-radius: 12px;">
     <p>本網站僅供個人作品使用，不提供商業用途</p>
-    <p>資料來源</p>
+
+    <router-link to="/source" class="no-underline hover:text-[#ff7e63e6]">
+      <p>資料來源</p>
+        </router-link>
   </div>
 
 </template>
--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -11,8 +11,11 @@ const items = ref([
   <div class="sticky top-0" style="z-index: 10001;">
     <Menubar class="mt-0 lg:mt-3 flex-col lg:flex-row" style="border-radius: 12px;">
       <template #start>
-        <h1 class="text-3xl">故事人物關係圖</h1>
+        <router-link to="/" class="text-3xl no-underline hover:text-[#ff7e63e6]">
+          <h1>故事人物關係圖</h1>
+        </router-link>
       </template>
+
       <template #end>
         <div class="flex items-center gap-2">
           <p class="text-xl font-bold">分類：動漫</p>
@@ -23,16 +26,18 @@ const items = ref([
 </template>
 <style>
 .p-menubar-root-list {
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    flex: 1 1 auto;
-    gap: var(--p-menubar-gap);
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex: 1 1 auto;
+  gap: var(--p-menubar-gap);
 }
+
 .p-menubar-end {
-    margin-left: 0px;
-  }
-@media(min-width: 1024px){
+  margin-left: 0px;
+}
+
+@media(min-width: 1024px) {
   .p-menubar-end {
     margin-left: auto;
   }
